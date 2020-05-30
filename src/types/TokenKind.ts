c318e89a3e7d4a396d3f9f9f5749a666731984a8
hota1024 <hotalog@hota1024.com>
2020-05-30 20:26:29 +0900

    🏷️ Update TokenKind type

--- src/types/TokenKind.ts
+++ src/types/TokenKind.ts
@@ -2,11 +2,40 @@
  * TokenKind type.
  */
 export type TokenKind =
+  // Literal
   | 'numeric_literal'
   | 'string_literal'
-  | 'symbol' // symbols(with operators)
-  | 'identifier' // names
-  | 'reserved' // reserved keywords
-  | 'new_line'
+  // Comments
   | 'inline_comment'
   | 'range_comment'
+  // Symbols
+  | 'equals_equals'
+  | 'exclamation_equals'
+  | 'greater_than'
+  | 'greater_than_equals'
+  | 'less_than'
+  | 'less_than_equals'
+  | 'plus'
+  | 'minus'
+  | 'astersik'
+  | 'astersik_asterisk'
+  | 'slash'
+  | 'equals'
+  | 'left_parenthesis'
+  | 'right_parenthesis'
+  | 'left_curly_braces'
+  | 'right_curly_braces'
+  | 'left_square_brackets'
+  | 'right_square_brackets'
+  | 'comma'
+  | 'semicolon'
+  // Keywords
+  | 'if'
+  | 'else'
+  | 'const'
+  | 'let'
+  | 'true'
+  | 'false'
+  // Others
+  | 'new_line'
+  | 'identifier'
