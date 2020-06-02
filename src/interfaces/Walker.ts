dd70a321de499fe6966aa3f41b8154c9be2ee7d0
hota1024 <hotalog@hota1024.com>
2020-06-02 18:58:09 +0900

    🏷️ Add Walker interface

---
+++ src/interfaces/Walker.ts
@@ -0,0 +1,47 @@
+/*
+ * WalkerPatternFn type.
+ */
+export type WalkerPatternFn<T> = (value: T) => boolean
+
+/*
+ * WalkerPattern type.
+ */
+export type WalkerPattern<T> = T | WalkerPatternFn<T>
+
+/*
+ * Walker interface.
+ */
+export interface IWalker<T> {
+  /**
+   * Returns current index.
+   */
+  index(): number
+
+  /**
+   * Returns value.
+   *
+   * @param offset offset index.
+   */
+  value(offset?: number): T
+
+  /**
+   * Forward index and returns value.
+   *
+   * @param steps Steps.
+   */
+  next(steps?: number): T
+
+  /**
+   * Returns value of the number of steps ahead.
+   *
+   * @param steps Steps.
+   */
+  peek(steps?: number): T | void
+
+  /**
+   * Returns whether next values matches patterns.
+   *
+   * @param patterns Patterns.
+   */
+  match(patterns: Iterable<WalkerPattern<T>>): boolean
+}
