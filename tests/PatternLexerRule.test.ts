ccf6b4998decebb73178537e62ce4a680503ae9b
hota1024 <hotalog@hota1024.com>
2020-06-05 23:08:46 +0900

    ✅ Add PatternLexerRule.test.ts

---
+++ tests/PatternLexerRule.test.ts
@@ -0,0 +1,70 @@
+import { PatternLexerRule } from '../src/impls'
+import { Walker, Loc } from '../src/classes'
+
+describe('PatternLexerRule class', () => {
+  test('PatternLexerRule#constructor', () => {
+    const rule = new PatternLexerRule([
+      {
+        pattern: '+',
+        kind: 'plus',
+      },
+      {
+        pattern: '==',
+        kind: 'equals_equals',
+      },
+    ])
+
+    expect(rule.patterns).toMatchObject([
+      {
+        pattern: '==',
+        kind: 'equals_equals',
+      },
+      {
+        pattern: '+',
+        kind: 'plus',
+      },
+    ])
+  })
+
+  test('PatternLexerRule#validate', () => {
+    const rule = new PatternLexerRule([
+      {
+        pattern: '+',
+        kind: 'plus',
+      },
+    ])
+
+    expect(rule.validate(new Walker('+'))).toBeTruthy()
+    expect(rule.validate(new Walker('!!!'))).toBeFalsy()
+  })
+
+  test('PatternLexerRule#execute', () => {
+    const rule = new PatternLexerRule([
+      {
+        pattern: '+',
+        kind: 'plus',
+      },
+      {
+        pattern: '-',
+        kind: 'minus',
+      },
+      {
+        pattern: '==',
+        kind: 'equals_equals',
+      },
+    ])
+
+    expect(rule.execute(new Walker('+'))).toMatchObject({
+      kind: 'plus',
+      loc: new Loc(0, 1),
+    })
+    expect(rule.execute(new Walker('-'))).toMatchObject({
+      kind: 'minus',
+      loc: new Loc(0, 1),
+    })
+    expect(rule.execute(new Walker('=='))).toMatchObject({
+      kind: 'equals_equals',
+      loc: new Loc(0, 2),
+    })
+  })
+})
