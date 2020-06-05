1237421608c034fe0bcf947adc506a5f75aa3b66
hota1024 <hotalog@hota1024.com>
2020-06-05 11:50:53 +0900

    🏷️ Add SymbolToken type

---
+++ src/types/tokens/SymbolToken.ts
@@ -0,0 +1,51 @@
+import {
+  EqualsToken,
+  ExclamationEqualsToken,
+  GreaterThanToken,
+  GreaterThanEqualsToken,
+  LessThanToken,
+  LessThanEqualsToken,
+  PlusToken,
+  MinusToken,
+  AsteriskToken,
+  AsteriskAsteriskToken,
+  SlashToken,
+  LeftParenthesisToken,
+  RightParenthesisToken,
+  LeftCurlyBracesToken,
+  RightCurlyBracesToken,
+  LeftSquareBracketsToken,
+  RightSquareBracketsToken,
+  CommaToken,
+  DotToken,
+  SemicolonToken,
+  ColonToken,
+  QuestionToken,
+} from '../'
+
+/*
+ * SymbolToken type.
+ */
+export type SymbolToken =
+  | EqualsToken
+  | ExclamationEqualsToken
+  | GreaterThanToken
+  | GreaterThanEqualsToken
+  | LessThanToken
+  | LessThanEqualsToken
+  | PlusToken
+  | MinusToken
+  | AsteriskToken
+  | AsteriskAsteriskToken
+  | SlashToken
+  | LeftParenthesisToken
+  | RightParenthesisToken
+  | LeftCurlyBracesToken
+  | RightCurlyBracesToken
+  | LeftSquareBracketsToken
+  | RightSquareBracketsToken
+  | CommaToken
+  | DotToken
+  | SemicolonToken
+  | ColonToken
+  | QuestionToken
