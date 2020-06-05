c3112270d32295884a99f7fa545aa27b1747d5ee
hota1024 <hotalog@hota1024.com>
2020-06-05 11:51:25 +0900

    🏷️ Update Token type

--- src/types/tokens/Token.ts
+++ src/types/tokens/Token.ts
@@ -1,24 +1,3 @@
-import { EqualsToken } from './EqualsToken'
-import { ExclamationEqualsToken } from './ExclamationEqualsToken'
-import { GreaterThanToken } from './GreaterThanToken'
-import { GreaterThanEqualsToken } from './GreaterThanEqualsToken'
-import { LessThanToken } from './LessThanToken'
-import { LessThanEqualsToken } from './LessThanEqualsToken'
-import { PlusToken } from './PlusToken'
-import { MinusToken } from './MinusToken'
-import { AsteriskToken } from './AsteriskToken'
-import { AsteriskAsteriskToken } from './AsteriskAsteriskToken'
-import { SlashToken } from './SlashToken'
-import { LeftParenthesisToken } from './LeftParenthesisToken'
-import { RightParenthesisToken } from './RightParenthesisToken'
-import { LeftCurlyBracesToken } from './LeftCurlyBracesToken'
-import { RightCurlyBracesToken } from './RightCurlyBracesToken'
-import { LeftSquareBracketsToken } from './LeftSquareBracketsToken'
-import { RightSquareBracketsToken } from './RightSquareBracketsToken'
-import { CommaToken } from './CommaToken'
-import { SemicolonToken } from './SemicolonToken'
-import { ColonToken } from './ColonToken'
-import { QuestionToken } from './QuestionToken'
 import { IfToken } from './IfToken'
 import { ElseToken } from './ElseToken'
 import { ConstToken } from './ConstToken'
@@ -27,9 +6,9 @@ import { TrueKeywordToken } from './TrueKeywordToken'
 import { FalseKeywordToken } from './FalseKeywordToken'
 import { NewLineToken } from './NewLineToken'
 import { IdentifierToken } from './IdentifierToken'
-import { DotToken } from './DotToken'
 import { LiteralToken } from './LiteralToken'
 import { CommentToken } from './CommentToken'
+import { SymbolToken } from './SymbolToken'
 
 /*
  * Token type.
@@ -37,30 +16,7 @@ import { CommentToken } from './CommentToken'
 export type Token =
   | LiteralToken
   | CommentToken
-  // Symbols
-  | EqualsToken
-  | ExclamationEqualsToken
-  | GreaterThanToken
-  | GreaterThanEqualsToken
-  | LessThanToken
-  | LessThanEqualsToken
-  | PlusToken
-  | MinusToken
-  | AsteriskToken
-  | AsteriskAsteriskToken
-  | SlashToken
-  | EqualsToken
-  | LeftParenthesisToken
-  | RightParenthesisToken
-  | LeftCurlyBracesToken
-  | RightCurlyBracesToken
-  | LeftSquareBracketsToken
-  | RightSquareBracketsToken
-  | CommaToken
-  | DotToken
-  | SemicolonToken
-  | ColonToken
-  | QuestionToken
+  | SymbolToken
   // Reserved keywords
   | IfToken
   | ElseToken
