ef883d9c12be2bd922bcb48acbf7056b4a776423
hota1024 <hotalog@hota1024.com>
2020-06-05 12:15:07 +0900

    🏷️ Add ReservedKeywordToken type

---
+++ src/types/tokens/ReservedKeywordToken.ts
@@ -0,0 +1,6 @@
+import { IfToken, ElseToken, ConstToken, LetToken } from '../'
+
+/*
+ * ReservedKeywordToken type.
+ */
+export type ReservedKeywordToken = IfToken | ElseToken | ConstToken | LetToken
