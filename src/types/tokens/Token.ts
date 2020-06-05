719da001d998331dd99db1a78168bd2f3fab6132
hota1024 <hotalog@hota1024.com>
2020-06-05 12:15:39 +0900

    🏷️ Update Token type

--- src/types/tokens/Token.ts
+++ src/types/tokens/Token.ts
@@ -1,7 +1,3 @@
-import { IfToken } from './IfToken'
-import { ElseToken } from './ElseToken'
-import { ConstToken } from './ConstToken'
-import { LetToken } from './LetToken'
 import { TrueKeywordToken } from './TrueKeywordToken'
 import { FalseKeywordToken } from './FalseKeywordToken'
 import { NewLineToken } from './NewLineToken'
