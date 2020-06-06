ac49f4518ec5218706f9aca1de286a986fb4f8fb
hota1024 <hotalog@hota1024.com>
2020-06-07 00:18:45 +0900

    🏷️ Update Lexer class

--- src/classes/Lexer.ts
+++ src/classes/Lexer.ts
@@ -1,5 +1,16 @@
 import { ILexer, ILexerRule } from '../interfaces'
 import { Token } from '../types/tokens'
+import {
+  InlineCommentLexerRule,
+  RangeCommentLexerRule,
+  IdentifierLexerRule,
+  NumericLiteralLexerRule,
+  StringLiteralLexerRule,
+  PatternLexerRule,
+  LexerInvalidCharError,
+} from '../impls'
+import { Walker } from './Walker'
+import { Loc } from './Loc'
 
 /*
  * Lexer class.
@@ -8,10 +19,69 @@ export class Lexer implements ILexer {
   /**
    * Lexer rules.
    */
-  rules: ILexerRule[] = []
+  rules: ILexerRule[] = [
+    new InlineCommentLexerRule(),
+    new RangeCommentLexerRule(),
+    new NumericLiteralLexerRule(),
+    new StringLiteralLexerRule(),
+    new PatternLexerRule((pattern) => [
+      pattern('==', 'equals_equals'),
+      pattern('!=', 'exclamation_equals'),
+      pattern('>', 'greater_than'),
+      pattern('>=', 'greater_than_equals'),
+      pattern('<', 'less_than'),
+      pattern('<=', 'less_than_equals'),
+      pattern('+', 'plus'),
+      pattern('+=', 'plus_equals'),
+      pattern('-', 'minus'),
+      pattern('-=', 'minus_equals'),
+      pattern('*', 'asterisk'),
+      pattern('*=', 'asterisk_equals'),
+      pattern('/', 'slash'),
+      pattern('/=', 'slash_equals'),
+      pattern('=', 'equals'),
+      pattern('(', 'left_parenthesis'),
+      pattern(')', 'right_parenthesis'),
+      pattern('{', 'left_curly_braces'),
+      pattern('}', 'right_curly_braces'),
+      pattern('[', 'left_square_brackets'),
+      pattern(']', 'right_square_brackets'),
+      pattern(',', 'comma'),
+      pattern('.', 'dot'),
+      pattern(';', 'semicolon'),
+      pattern(':', 'colon'),
+      pattern('?', 'question'),
+      pattern('if', 'if'),
+      pattern('else', 'else'),
+      pattern('const', 'const'),
+      pattern('let', 'let'),
+      pattern('true', 'true'),
+      pattern('false', 'false'),
+      pattern('\n', 'new_line'),
+    ]),
+    new IdentifierLexerRule(),
+  ]
 
   tokenize(source: string): Token[] {
     const tokens: Token[] = []
+    const walker = new Walker(source)
+
+    while (!walker.done()) {
+      if (walker.value() === ' ') {
+        walker.next()
+      } else {
+        const rule = this.rules.find((rule) => rule.validate(walker))
+
+        if (typeof rule === 'undefined') {
+          throw new LexerInvalidCharError(
+            walker.value(),
+            new Loc(walker.index(), walker.index() + 1)
+          )
+        } else {
+          tokens.push(rule.execute(walker))
+        }
+      }
+    }
 
     return tokens
   }
