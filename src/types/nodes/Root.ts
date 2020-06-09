d97d88f4dfbc2f4dbdcfdb0f3c9bec8e6af429c4
hota1024 <hotalog@hota1024.com>
2020-06-09 19:03:15 +0900

    🏷️ Update Root type

--- src/types/nodes/Root.ts
+++ src/types/nodes/Root.ts
@@ -1,6 +1,12 @@
 import { NodeBase } from './NodeBase'
+import { StatementNode } from './StatementNode'
 
 /*
  * Root type.
  */
-export type Root = NodeBase<'root'>
+export type Root = NodeBase<'root'> & {
+  /**
+   * Statements array.
+   */
+  statements: StatementNode[]
+}
