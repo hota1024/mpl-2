75f4082546b6d62be4c0c0d3e15b010435720e1f
hota1024 <hotalog@hota1024.com>
2020-06-09 17:51:56 +0900

    🏷️ Update Node type

--- src/types/nodes/Node.ts
+++ src/types/nodes/Node.ts
@@ -1,7 +1,8 @@
 import { ExpressionNode } from './ExpressionNode'
 import { StatementNode } from './StatementNode'
+import { OtherNode } from './OtherNode'
 
 /*
  * Node type.
  */
-export type Node = ExpressionNode | StatementNode
+export type Node = ExpressionNode | StatementNode | OtherNode
