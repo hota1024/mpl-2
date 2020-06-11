c0c9cf013173abade622359b47e70a00cc674d3a
hota1024 <hotalog@hota1024.com>
2020-06-11 22:21:26 +0900

    🏷️ Add FnToken type

--- src/types/tokens/ReservedKeywordToken.ts
+++ src/types/tokens/ReservedKeywordToken.ts
@@ -1,6 +1,12 @@
 import { IfToken, ElseToken, ConstToken, LetToken } from '../'
+import { FnToken } from './FnToken'
 
 /*
  * ReservedKeywordToken type.
  */
-export type ReservedKeywordToken = IfToken | ElseToken | ConstToken | LetToken
+export type ReservedKeywordToken =
+  | IfToken
+  | ElseToken
+  | ConstToken
+  | LetToken
+  | FnToken
