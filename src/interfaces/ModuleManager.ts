54248612ab06d0f9babd0deca9644b23aa8f679d
hota1024 <hotalog@hota1024.com>
2020-06-14 12:58:57 +0900

    🏷️ Update ModuleManager interface

--- src/interfaces/ModuleManager.ts
+++ src/interfaces/ModuleManager.ts
@@ -1,5 +1,5 @@
-import { Module } from '../abstracts'
 import { IRuntime } from './Runtime'
+import { IModule } from './Module'
 
 /*
  * ModuleManager interface.
@@ -11,14 +11,14 @@ export interface IModuleManager {
    * @param id Module ID.
    * @param module Module.
    */
-  register(id: string, module: Module)
+  register(id: string, module: IModule)
 
   /**
    * Register a module.
    *
    * @param module Module.
    */
-  register(module: Module)
+  register(module: IModule)
 
   /**
    * Load module to runtime.
@@ -26,5 +26,13 @@ export interface IModuleManager {
    * @param id Module ID.
    * @param runtime Runtime.
    */
-  loadTo(id: string, runtime: IRuntime): this
+  loadTo(id: string, runtime: IRuntime): void
+
+  /**
+   * Unload module from runtime.
+   *
+   * @param id Module ID.
+   * @param runtime Runtime.
+   */
+  unloadFrom(id: string, runtime: IRuntime): void
 }
